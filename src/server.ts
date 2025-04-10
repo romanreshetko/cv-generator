1f531f992c0886e20db93772573fd2e1615b94ee
Roman Reshetko <rvreshetko@edu.hse.ru>
2025-04-10 18:19:27 +0300

    added example download

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer"
 import cors from 'cors'
 import { generateResume } from "./handlers/generateResume";
+import { getExample } from "./handlers/getExample";
 
 const app = express();
 const upload = multer({storage: multer.memoryStorage()});
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(cors({origin: 'http://localhost:5173'}))
 
 app.post('/generate', upload.single("file"), generateResume);
+
+app.get('/example', getExample);
 
 app.listen(port, () => {
     console.log("Server is running on port 3000");
